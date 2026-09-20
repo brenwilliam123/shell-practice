e28dac1ef3ec91f41563522133910a16b4511ab9
brenham123 <brenwilliam1303@gmail.com>
2026-09-20 16:53:46 +0100

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1="$1" #there should be no spaces around the equal sign
+Person2="$2"
+
+echo "$Person1: Hey $Person2, how are you?"
+echo "$Person2: I'm doing well, $Person1. How about you?"
+echo "$Person1: I'm good too. What have you been up to lately?"  
+echo "$Person2: I've been busy with work and spending time with my family. How about you?"
