d567101fa6877dc83a824a3436b4310df8d14ba0
brenham123 <brenwilliam1303@gmail.com>
2026-09-20 16:21:00 +0100

    variables

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1="William"
+Person2="Stalin"
+
+echo "$Person1: Hey $Person2, how are you?"
+echo "$Person2: I'm doing well, $Person1. How about you?"
+echo "$Person1: I'm good too. What have you been up to lately?"  
+echo "$Person2: I've been busy with work and spending time with my family. How about you?"
