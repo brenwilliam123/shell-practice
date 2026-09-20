ecb190589d220a961c645a1d5c888e64fb2d812a
brenham123 <brenwilliam1303@gmail.com>
2026-09-20 17:21:00 +0100

    variables

---
+++ 07-array.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Create an array
+fruits=("apple" "banana" "orange")
+
+# Access array elements
+echo "First fruit: ${fruits[0]}"
+echo "Second fruit: ${fruits[1]}"
+echo "Third fruit: ${fruits[2]}"
+
+# Get the length of the array
+length=${#fruits[@]}
+echo "Number of fruits: $length"
