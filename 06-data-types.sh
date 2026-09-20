8e591c8a7a10e12eaa13605e19305847c2e6ce4d
brenham123 <brenwilliam1303@gmail.com>
2026-09-20 17:10:11 +0100

    variables

---
+++ 06-data-types.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Number1=10
+Number2=20
+
+TIMESTAMP=$(date) #this is a variable that stores the current timestamp
+echo "Script executed at: $TIMESTAMP"
+
+#Sum=$((Number1+Number2))                              
