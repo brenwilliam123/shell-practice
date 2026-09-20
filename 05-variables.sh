89910fa70f9f0ea58246ebe74b7dfd15717468b2
brenham123 <brenwilliam1303@gmail.com>
2026-09-20 17:01:08 +0100

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "Enter your pin number: :"
-read pin #here pin is a variable that stores the user input
+read -s pin #here pin is a variable that stores the user input
 echo "Your number is: $pin"
